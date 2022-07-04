8593271e486172d4b6b3e1aa65fa34cc45b0ffae
Julian Wiche <julian@wiche.eu>
2022-07-04 11:24:16 +0200

    removed styles... that's whayt one should do in the lection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import IndecisionApp from './components/IndecisionApp';
-import './styles/styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
