b71863ada13bb252aeb1abf4a7410cbedecaa436
Julian Wiche <julian@wiche.eu>
2022-07-11 11:38:42 +0200

    Switch from indescicion app to boilder plate => "initial"

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import IndecisionApp from './components/IndecisionApp';
+
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <IndecisionApp />
+    <p>Boilderplate</p>
 );
